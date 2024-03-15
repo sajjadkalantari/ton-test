0abc1591d48e3d56eda4afc58edb08bf47b80f52
sajad <sajadkalantari@outlook.com>
2024-03-15 09:45:06 +0330

    add hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { HashRouter, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Home from "./Home";
 import { LeaderBoard } from './components/LeaderBoard';
 import Slides from './components/Slides';
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />}  />
         <Route path="/leader" element={<LeaderBoard />}  />
         <Route path="/story/:id" element={<Slides />}  />
       </Routes>
-    </Router>
+    </HashRouter>
 
   );
 }
