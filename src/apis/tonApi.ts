4815595fdea61ecca624596c6ade2a56375a65f2
sajad <sajadkalantari@outlook.com>
2024-03-18 07:40:19 +0330

    ton main api

--- src/apis/tonApi.ts
+++ src/apis/tonApi.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://testnet.tonapi.io';
+const API_BASE_URL = 'https://tonapi.io';
 const tonApi = axios.create({
     baseURL: API_BASE_URL,
 });
