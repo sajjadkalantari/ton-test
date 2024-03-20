fc0367eb439a22f2b2affb1dea0356a06bf85d6f
sajad <sajadkalantari@outlook.com>
2024-03-20 22:07:41 +0330

    fix leader board bg

--- src/components/styled/styled.tsx
+++ src/components/styled/styled.tsx
@@ -200,7 +200,7 @@ export const TopPlayersContainer = styled.div`
   // margin-bottom: 10px;
   padding: 20px 0px;
   box-sizing: border-box;
-  background-image: linear-gradient(to bottom, rgba(0,0,0,0.5) 50%, rgba(0,0,0,1) 100%), url('/bg3.svg');
+  background-image: linear-gradient(to bottom, rgba(0,0,0,0.5) 50%, rgba(0,0,0,1) 100%), url('./bg3.svg');
   background-size: cover;
   background-position: center;
 `;
